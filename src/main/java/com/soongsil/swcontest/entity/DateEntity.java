f6c08b0ff13f7bf31f9b07af38a3acebd935dcb0
TaeWoonJeong <pig07911@naver.com>
2022-07-14 01:19:50 +0900

    스웨거 문서에 parameters 삭제

--- src/main/java/com/soongsil/swcontest/entity/DateEntity.java
+++ src/main/java/com/soongsil/swcontest/entity/DateEntity.java
@@ -1,5 +1,7 @@
 package com.soongsil.swcontest.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -10,10 +12,13 @@ import java.time.LocalDateTime;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@Getter
 public class DateEntity {
     @CreatedDate
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdAt;
 
     @LastModifiedDate
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updatedDate;
 }
