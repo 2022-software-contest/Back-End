46d1823ab7fff5b0cccc96ba4e265698bd12aa49
TaeWoonJeong <pig07911@naver.com>
2022-08-29 22:00:02 +0900

    feat: transform 추가

---
+++ src/main/java/com/soongsil/swcontest/config/Workaround.java
@@ -0,0 +1,33 @@
+package com.soongsil.swcontest.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.stereotype.Component;
+import springfox.documentation.oas.web.OpenApiTransformationContext;
+import springfox.documentation.oas.web.WebMvcOpenApiTransformationFilter;
+import springfox.documentation.spi.DocumentationType;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class Workaround implements WebMvcOpenApiTransformationFilter {
+
+    @Override
+    public OpenAPI transform(OpenApiTransformationContext<HttpServletRequest> context) {
+        OpenAPI openApi = context.getSpecification();
+        Server server = openApi.getServers().get(0);
+
+        if (server.getUrl().contains(":80")) {
+            server.setUrl(server.getUrl().replace(":80",""));
+        }
+        if (!server.getUrl().contains("https")) {
+            server.setUrl(server.getUrl().replace("http","https"));
+        }
+        return openApi;
+    }
+
+    @Override
+    public boolean supports(DocumentationType documentationType) {
+        return documentationType.equals(DocumentationType.OAS_30);
+    }
+}
